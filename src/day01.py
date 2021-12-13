21b27bf2a67003cf19b457dd1e52a4e047ddb1d2
rjuchnicki <1916201+rjuchnicki@users.noreply.github.com>
2021-12-12 23:44:55 -0500

    Day 1 Python

---
+++ src/day01.py
@@ -0,0 +1,29 @@
+
+
+def part1(depths):
+    increases = 0
+    for i in range(1, len(depths)):
+        if depths[i] > depths[i-1]:
+            increases += 1
+
+    return increases
+
+def part2(depths):
+    increases = 0
+    for i in range(3, len(depths)):
+        shared = depths[i-1] + depths[i-2]
+        if shared + depths[i-3] < shared + depths[i]:
+            increases += 1
+
+    return increases
+
+def main():
+    with open('../data/day01.txt') as f:
+        depths = [int(depth.strip()) for depth in f.readlines()]
+
+    print(part1(depths))
+    print(part2(depths))
+
+
+if __name__ == '__main__':
+    main()
